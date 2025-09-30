967e7fcc8516cfeec048c389bbdaca1fad8af281
Yoni Segal <yrsegal@gmail.com>
2025-09-30 15:45:31 -0400

    implement a couple of puzzle skips

---
+++ injection-blackshard-pushing.rb
@@ -0,0 +1,35 @@
+
+InjectionHelper.defineMapPatch(323, 52) { |event|
+  event.patch(:blackshardskippuzzle) { |page|
+    choices = page.lookForSequence(
+      [:ShowChoices, ["Yes", "No"], 2],
+      [:When, 0, "Yes"],
+      [:When, 1, "No"],
+      :BranchEndChoices)
+
+    if choices
+      choices[0].parameters[0].push("Skip Puzzle")
+      page.insertBefore(choices[3],
+        [:When, 2, "Skip Puzzle"],
+          [:ShowText, "Do you want to skip this puzzle completely?"],
+          [:ShowChoices, ["No", "Yes"], 1],
+          [:When, 0, "No"],
+            :ExitEventProcessing,
+          :Done,
+          [:When, 1, "Yes"],
+            *[[26, 58, 59, :Left], [53, 62, 62, :Up], [54, 58, 66, :Right], 
+              [55, 44, 60, :Up], [61, 50, 61, :Up]].map { |shard, targetX, targetY| 
+              [:SetEventLocation, shard, :Constant, targetX, targetY, :Down] 
+            },
+            [:ConditionalBranch, :Variable, 96, :Constant, :>=, 66],
+              [:SetEventLocation, 50, :Constant, 48, 65, :Right],
+            :Else,
+              [:SetEventLocation, 50, :Constant, 48, 65, :Down],
+            :Done,
+          :Done,
+        :Done)
+    end
+
+    next choices
+  }
+}
---
+++ injection-blacksteeple-robots.rb
@@ -0,0 +1,48 @@
+BLACKSTEEPLE_ROBOTS = [[31, 53, 76], [32, 67, 69], [35, 78, 69]]
+
+BLACKSTEEPLE_ROBOTS.each { |robot, targetX, targetY|
+  InjectionHelper.defineMapPatch(443, robot) { |event|
+    event.patch(:blacksteeplerobots) { |page|
+      matched = page.lookForAll([:ControlSelfSwitch, 'A', true])
+
+      for insn in matched
+        page.insertAfter(insn, 
+          [:ShowText, "(Do you want to automatically move the robot to the correct location?)"],
+          [:ShowChoices, ["Yes", "No"], 2],
+          [:When, 0, "Yes"],
+            [:SetEventLocation, :This, :Constant, targetX, targetY, :Down],
+          :Done,
+          [:When, 1, "No"],
+          :Done)
+      end
+
+      next !matched.empty?
+    }
+  }
+}
+
+InjectionHelper.defineMapPatch(443, 107) { |event|
+  event.patch(:blacksteeplerobotreset) { |page|
+    choices = page.lookForSequence(
+      [:ShowChoices, ["Yes", "No"], 2],
+      [:When, 0, "Yes"],
+      [:When, 1, "No"],
+      :BranchEndChoices)
+
+    if choices
+      choices[0].parameters[0].push("Skip Puzzle")
+      page.insertBefore(choices[3],
+        [:When, 2, "Skip Puzzle"],
+          [:ShowText, "Do you want to skip this puzzle completely?"],
+          [:ShowChoices, ["No", "Yes"], 1],
+          [:When, 0, "No"],
+          :Done,
+          [:When, 1, "Yes"],
+            *BLACKSTEEPLE_ROBOTS.map { |robot, targetX, targetY| [:SetEventLocation, robot, :Constant, targetX, targetY, :Down] },
+          :Done,
+        :Done)
+    end
+
+    next choices
+  }
+}
