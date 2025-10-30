2b3fee6353df7df960ae13888678123ea05f0f3e
Yoni Segal <yrsegal@gmail.com>
2025-10-30 19:05:24 -0400

    fix tale of two hearts skip

--- injection-tale-of-hearts.rb
+++ injection-tale-of-hearts.rb
@@ -1,13 +1,15 @@
 InjectionHelper.defineMapPatch(431) { |map|
-  map.createSinglePageEvent(18, 41, "Accessibility Option") { |page|
+  map.createSinglePageEvent(18, 14, "Accessibility Option") { |page|
+    page.setGraphic("trchar039")
     page.interact(
       [:ShowText, "Do you want to skip this stealth segment?"],
+      [:ShowChoices, ["Yes", "No"], 2],
       [:When, 0, "Yes"],
         [:PlaySoundEvent, 'Exit Door', 100, 100],
         [:ChangeScreenColorTone, Tone.new(-255,-255,-255,0), 10],
         [:Wait, 10],
         [:TransferPlayer, :Constant, 329, 42, 13, :Down, false],
-        [:Script, 'advanceQuestSilent(:TOH,5,colorQuest("Blue")'],
+        [:Script, 'advanceQuestSilent(:TOH,5,colorQuest("Blue"))'],
         [:ChangeScreenColorTone, Tone.new(0,0,0,0), 10],
         [:Wait, 10],
       :Done,
