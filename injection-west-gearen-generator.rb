cb9215552712cd639b0fbdc2226bdcee90bd5e59
Yoni Segal <yrsegal@gmail.com>
2025-11-05 09:02:18 -0500

    catch (I think) the last of these

--- injection-west-gearen-generator.rb
+++ injection-west-gearen-generator.rb
@@ -6,6 +6,7 @@ InjectionHelper.defineMapPatch(256, 41, 0) { |page|
       [:ShowText, "You feel an invisible hand of electricity tugging yours!"],
       [:ShowText, "For some reason, the Rotom wants to escort you past the stealth segment."],
       [:ShowText, "Do you want to let it skip the segment for you?"],
+      [:ShowChoices, ["Yes", "No"], 2],
       [:When, 0, "Yes"],
         [:ChangeScreenColorTone, Tone.new(-255,-255,-255,0), 10],
         [:PlaySoundEvent, 'Exit Door', 100, 100],
