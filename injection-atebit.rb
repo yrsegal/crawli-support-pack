608076ece446826407b37e0afa216c65e2deca74
Yoni Segal <yrsegal@gmail.com>
2025-12-26 17:45:16 -0500

    Add guardrails for atebit cave

---
+++ injection-atebit.rb
@@ -0,0 +1,26 @@
+InjectionHelper.defineMapPatch(521) { |map|
+
+  map.fillArea(33, 41,
+    ["  A                        ",
+     "                           ",
+     "                           ",
+     "                          A",
+     "                           ",
+     "                          A",
+     "           BB              ",
+     "           CC              ",
+     "                           ",
+     "                           ",
+     "                           ",
+     "                           ",
+     "                           ",
+     "                           ",
+     "                           ",
+     "                           ",
+     "AAA                        "],
+    {
+      "A" => [1008, nil, nil],
+      "B" => [1025, 923, nil],
+      "C" => [1025, nil, nil],
+    })
+}
